c24cc29beaa13e6425577220de62982282a89308
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2022-03-25 11:37:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    HOST: "109.94.209.66",
+    PORT: 3306,
+    // SOCKET: 'mysql.sock', //If you need socket path
+    DBUSER: "admin6247k",
+    DBPASSWORD: "lao6247K",
+    DBNAME: "trainlist",
+    jwt: 'yourjwtkey' //Your jwt key for token generate
+}
